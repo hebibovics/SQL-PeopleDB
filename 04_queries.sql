c065857394e8dee9da6ab4e3b72094923de3f8d0
hebibovics <126088168+hebibovics@users.noreply.github.com>
2024-11-16 11:18:16 +0100

    Update 04_queries.sql

--- 04_queries.sql
+++ 04_queries.sql
@@ -12,3 +12,15 @@ GO
 
 SELECT * FROM dbo.Phone;
 GO
+
+
+SELECT p.FirstName, p.LastName, j.JobTitle, j.StartDate, j.EndDate
+FROM dbo.Person p
+INNER JOIN dbo.Job j
+ON p.PersonID = j.PersonID;
+
+
+SELECT p.FirstName, p.LastName, j.JobTitle, j.StartDate, j.EndDate, a.Street, a.City, a.Country
+FROM dbo.Person p
+INNER JOIN dbo.Job j ON p.PersonID = j.PersonID
+INNER JOIN dbo.Address a ON p.PersonID = a.PersonID;
